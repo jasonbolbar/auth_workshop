8eac1b7559218d6c05371cd6cf807bddfd5f6ebc
Jason Bolaños <jbolanos@growthaccelerationpartners.com>
2019-06-13 08:50:20 -0600

    Remove Facebook credentials

--- www/html/facebook_oauth/Core/Credentials.php
+++ www/html/facebook_oauth/Core/Credentials.php
@@ -2,8 +2,8 @@
 
 class Credentials
 {
-	const FACEBOOK_APP_ID= '299073131005192';
-	const FACEBOOK_APP_SECRET = '4638d0fca7457ee8ae868c688059b13b';
+	const FACEBOOK_APP_ID= '<application id goes here>';
+	const FACEBOOK_APP_SECRET = '<client_secret_goes_here>';
 	const AUTHORIZE_URL = 'https://www.facebook.com/v3.3/dialog/oauth';
 	const TOKEN_URL = 'https://graph.facebook.com/v3.3/oauth/access_token';
 	const API_BASE = 'https://graph.facebook.com';
